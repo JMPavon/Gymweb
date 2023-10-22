df64247d5d8b5dcc8baae368fb1b176070dd1e2a
Jose Pavon <49964843+JMPavon@users.noreply.github.com>
2023-10-21 18:04:00 -0600

    Update @admin

--- main/admin.py
+++ main/admin.py
@@ -4,18 +4,23 @@ from .models import *
 # Register your models here.
 #yo
 
+@admin.register(Sucursal)
 class SucursalAdmin(admin.ModelAdmin):
     list_display = ("id", "nombre")
     search_fields = ("nombre",)
 
+#admin.site.register(Sucursal, SucursalAdmin)
+
+@admin.register(Genero)
 class GeneroAdmin(admin.ModelAdmin):
     list_display = ("id", "nombre")
     search_fields = ("nombre",)
 
+@admin.register(Evento)
 class EventosAdmin(admin.ModelAdmin):
     list_display = ('id', "nombreEvento", "fechaInicio", "fechaFin")
     search_fields = ("nombreEvento",)
 
-admin.site.register(Sucursal, SucursalAdmin)
-admin.site.register(Genero, GeneroAdmin)
-admin.site.register(Evento, EventosAdmin)
+
+
+
